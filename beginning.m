c39a71048a84ab0055f5957203f53ecd959326ea
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2022-06-21 11:54:09 -0400

    Update beginning.m

--- beginning.m
+++ beginning.m
@@ -1,16 +1,19 @@
-syms t
+%syms t
 
-sig = @(t)1000*cos(880*pi*t); 
+%sig = @(t)1000*cos(880*pi*t); 
 
-% fplot(sig*heaviside(t))
+%fplot(sig*heaviside(t))
 
-A = 0.5; 
+signalFrequency = 880*pi;
+
+A = 0.5;
+twoA = 2*A; 
 B = 100; 
 L = 100; 
 c = 333.33; 
 
 D_a = sqrt(B^2+(L-A)^2);
-D_2a = sqrt(B^2+(L-2*A)^2);
+D_2a = sqrt(B^2+(L-twoA)^2);
 
 tau1 = c/D_a; 
 tau2 = c/D_2a;
@@ -18,8 +21,37 @@ tau2 = c/D_2a;
 lowerBound = min(tau1,tau2)-0.01; 
 upperBound = max(tau1, tau2)+0.01;
 
+fs = 44100; 
+inverseFs = 1/fs;
+
+t = lowerBound:inverseFs:upperBound;
+signal = @(t)1000*cos(signalFrequency*t);
+
+subplot(2,1,1);
+plot(t,signal(t-tau1))
+
+subplot(2,1,2); 
+plot(t,signal(t-tau2))
 
 
-function [one] = lab1sim(A, B, L, sig)
+function [y1sig,y2sig] = lab1sim(A, B, L, sig)
+
+twoA = 2*A; 
+c = 333.3;
+D_a = sqrt(B^2+(L-A)^2);
+D_2a = sqrt(B^2+(L-twoA)^2);
+tau1 = c/D_a; 
+tau2 = c/D_2a;
+
+lowerBound = min(tau1,tau2)-0.01; 
+upperBound = max(tau1, tau2)+0.01;
+
+fs = 44100; 
+inverseFs = 1/fs;
+
+t = lowerBound:inverseFs:upperBound;
+
+y1sig = sig(t-tau1);
+y2sig = sig(t-tau2);
 
 end
